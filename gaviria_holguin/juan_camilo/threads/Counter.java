e2cb84c765ef61e2776ed87d7abbbf4219bd0e75
SURAMERICANA\juangaho <juan.gaviria@ceiba.com.co>
2017-07-31 16:28:24 -0500

    Se agrega clase Counter para pruebas de hilos

---
+++ gaviria_holguin/juan_camilo/threads/Counter.java
@@ -0,0 +1,17 @@
+package gaviria_holguin.juan_camilo.threads;
+
+public class Counter {
+    private byte count;
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public byte getCount() {
+        return count;
+    }
+}
